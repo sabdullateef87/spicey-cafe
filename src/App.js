45f03bc0ce0aef00d2d45c048fd75d812ab8b0a0
unknown <sabdullaeef87@gmail.com>
2020-03-19 13:01:49 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
+import LandingPage from "../src/Components/LandingPage"
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>First of his kind</h1>
+      <LandingPage />
     </div>
   );
 }
---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LandingPage() {
+  return <>This is the landing page</>;
+}
+
+export default LandingPage;
